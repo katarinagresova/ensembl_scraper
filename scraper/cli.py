5b6f01d0bfa9123bc5c513cb44f6bb3e9d37ec18
Katarína Grešová <514001@mail.muni.cz>
2021-09-02 19:23:30 +0200

    refactor cli module

--- scraper/cli.py
+++ scraper/cli.py
@@ -17,12 +17,12 @@ def verify_input(user_input: str, supported_names) -> list:
     return parts
 
 
-def get_user_inputs():
+def print_banner() -> None:
     ascii_banner = pyfiglet.figlet_format("Ensembl scraper")
     print(ascii_banner)
 
-    user_input = dict()
 
+def get_organisms_from_user() -> list:
     while True:
 
         print("Please select organisms you are interested in. Supported organisms are: "
@@ -36,8 +36,14 @@ def get_user_inputs():
 
     print('')
     print("Organisms selected: " + str(organisms))
+
+    return organisms
+
+
+def get_features_from_user(organisms: list) -> dict:
     print("Now you can select feature classes for each organism.")
 
+    user_input = dict()
     for organism in organisms:
 
         while True:
@@ -55,6 +61,21 @@ def get_user_inputs():
 
         user_input[organism] = features
 
+    return user_input
+
+
+def get_root_dir_from_user() -> str:
+    print('One more thing.')
+    while True:
+        root_dir = input('Please enter path to directory, where this program can create folders and store data: ')
+        if Path(root_dir).exists():
+            break
+        print("Path doesn't exist. Please try again.")
+        print('')
+    return root_dir
+
+
+def print_selected_settings(user_input: dict) -> None:
     print('')
     print('=========================')
     print('Selected settings')
@@ -62,17 +83,20 @@ def get_user_inputs():
     print(yaml.dump(user_input))
     print('')
 
-    print('One more thing.')
 
-    while True:
-        root_dir = input('Please enter path to directory, where this program can create folders and store data: ')
-        if Path(root_dir).exists():
-            break
-        print("Path doesn't exist. Please try again.")
-        print('')
-
+def do_final_confirm() -> None:
     print('')
     input('All done. Confirm by any input to start.')
     print('')
+
+
+def get_user_inputs():
+
+    print_banner()
+    organisms = get_organisms_from_user()
+    user_input = get_features_from_user(organisms)
+    print_selected_settings(user_input)
+    root_dir = get_root_dir_from_user()
+    do_final_confirm()
 
     return user_input, root_dir
