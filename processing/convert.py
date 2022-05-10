b716226137f62bcdc6d90d019135a96aa3699973
katarinagresova <514001@mail.muni.cz>
2022-05-10 14:57:00 +0200

    structure for processing module

---
+++ processing/convert.py
@@ -0,0 +1,24 @@
+
+# convert bed format to csv format
+def bed_to_csv(bed_file, csv_file):
+    pass
+
+# convert bed to fasta
+def bed_to_fasta(bed_path, fasta_path):
+    pass
+
+# convert fasta to bed
+def fasta_to_bed(fasta_path, bed_path):
+    pass
+
+# convert fasta to csv
+def fasta_to_csv(fasta_path, csv_file):
+    pass
+
+# convert csv to bed
+def csv_to_bed(csv_file, bed_file):
+    pass
+
+# convert csv to fasta
+def csv_to_fasta(csv_file, fasta_file):
+    pass
